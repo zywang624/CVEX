156ebddeea5011e8f404c879b70b61d92643ac52
vincent <891922758@qq.com>
2024-11-16 00:36:24 +0000

    feat: add setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+sudo apt install python3.10-venv -y
+
+python3 -m venv venv
+source venv/bin/activate
+
+sudo apt update
+sudo apt install git-lfs -y
+git lfs pull
+pip install -e .
+
+wget https://releases.hashicorp.com/vagrant/2.4.1/vagrant-2.4.1-1.x86_64.rpm
+sudo apt install alien -y
+sudo alien -i vagrant-2.4.1-1.x86_64.rpm
+
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible
+
+wget https://download.virtualbox.org/virtualbox/7.0.20/virtualbox-7.0_7.0.20-163906~Ubuntu~jammy_amd64.deb
+sudo dpkg -i virtualbox-7.0_7.0.20-163906~Ubuntu~jammy_amd64.deb
+sudo apt --fix-broken install -y
+sudo dpkg -i virtualbox-7.0_7.0.20-163906~Ubuntu~jammy_amd64.deb
+wget https://download.virtualbox.org/virtualbox/7.0.20/Oracle_VM_VirtualBox_Extension_Pack-7.0.20.vbox-extpack
+sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.20.vbox-extpack
+
